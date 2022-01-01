78c888462d68f0bf323862d54552356d53f5790d
bugsounet <github@bugsounet.fr>
2022-01-01 18:46:24 +0100

    prepare

--- EXT-MusicPlayer.js
+++ EXT-MusicPlayer.js
@@ -10,14 +10,24 @@ logMUSIC = (...args) => { /* do nothing */ }
 Module.register("EXT-MusicPlayer", {
   defaults: {
     debug: true,
+    useUSB: false,
+    musicPath: "/home/pi/Music",
+    checkSubDirectory: false,
+    autoStart: false,
+    minVolume: 30,
+    maxVolume: 100
   },
 
   start: function () {
     if (this.config.debug) logMUSIC = (...args) => { console.log("[MUSIC]", ...args) }
+    this.Music = new Music(this.config, (noti,params) => console.log("[MUSIC]", noti, params), this.config.debug)
+    this.initializeMusicVolumeVLC()
   },
 
   getScripts: function() {
-    return [ ]
+    return [
+      "/modules/EXT-MusicPlayer/components/musicplayer.js"
+    ]
   },
 
   getStyles: function () {
@@ -43,5 +53,35 @@ Module.register("EXT-MusicPlayer", {
 
   socketNotificationReceived: function(noti, payload) {
     
+  },
+
+  /** initialise Music volume control for VLC **/
+  initializeMusicVolumeVLC: function() {
+    /** convert volume **/
+    try {
+      let valueMin = null
+      valueMin = parseInt(this.config.minVolume)
+      if (typeof valueMin === "number" && valueMin >= 0 && valueMin <= 100) this.config.minVolume = ((valueMin * 255) / 100).toFixed(0)
+      else {
+        console.error("[MUSIC] config.music.minVolume error! Corrected with 30")
+        this.config.minVolume = 70
+      }
+    } catch (e) {
+      console.error("[MUSIC] config.music.minVolume error!", e)
+      this.config.minVolume = 70
+    }
+    try {
+      let valueMax = null
+      valueMax = parseInt(this.config.maxVolume)
+      if (typeof valueMax === "number" && valueMax >= 0 && valueMax <= 100) this.config.maxVolume = ((valueMax * 255) / 100).toFixed(0)
+      else {
+        console.error("[MUSIC] config.music.maxVolume error! Corrected with 100")
+        this.config.maxVolume = 255
+      }
+    } catch (e) {
+      console.error("[MUSIC] config.music.maxVolume error!", e)
+      this.config.maxVolume = 255
+    }
+    console.log("[MUSIC] VLC Volume Control initialized!")
   }
 })
